fe54f52040255ef6c12c99429d4d4804d5c1de2e
Andy Zhou <zhoua2001@gmail.com>
2026-03-20 11:11:48 -0400

    add basic seeking

--- Views/SeekbarView.axaml.cs
+++ Views/SeekbarView.axaml.cs
@@ -26,7 +26,22 @@ public partial class SeekbarView : UserControl
     private void OnSeekbarPointerPress(object? sender, PointerPressedEventArgs e)
     {
         long pos = (long)(e.GetPosition(this).X);
-        Debug.WriteLine("Seekbar pressed at: " + pos);
+        if (DataContext is ViewModels.MainWindowViewModel vm)
+        {
+            (int index, long position) = vm.Timeline.GetVideoChunkAtPosition(pos);
+            if (index == vm.Timeline.VideoTimeline.Count)
+            {
+                vm.VideoPlayer.Play(vm.Timeline.VideoTimeline[index - 1]);
+                long msPosition = vm.Timeline.VideoTimeline[index - 1].getDuration();
+                vm.VideoPlayer.JumpTo(msPosition);
+            } 
+            else
+            {
+                vm.VideoPlayer.Play(vm.Timeline.VideoTimeline[index]);
+                long msPosition = vm.Timeline.VideoTimeline[index].getDuration() * position / vm.Timeline.VideoTimeline[index].Length;
+                vm.VideoPlayer.JumpTo(msPosition);
+            }
+        }
     }
 
     public void DragOver(object? sender, DragEventArgs e)
